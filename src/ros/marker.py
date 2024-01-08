2b0cec4d2fbc463062d56c0c4d6c542e106fbfe9
Yoanngr <you@example.com>
2024-01-08 10:43:48 +0100

    Update Maker.py

--- src/ros/marker.py
+++ src/ros/marker.py
@@ -2,17 +2,32 @@
 
 import rospy
 from visualization_msgs.msg import Marker, MarkerArray
+import json
+
+def load_json(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
 
 def main():
     rospy.init_node('marker_publisher')
     vis_pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=0)
     rate = rospy.Rate(1)  # 1 Hz
 
+    json_data = load_json('/home/yoann/catkin_ws/src/test/src/bonding.json')
+
+    magnet_length = json_data["Magnet"]["length"]
+    magnet_width = json_data["Magnet"]["width"]
+    magnet_thickness = json_data["Magnet"]["thickness"]
+
+    picking_data = json_data["picking"]
+    placing_data = json_data["placing"]
+
     while not rospy.is_shutdown():
         marker_array = MarkerArray()
 
-        rows = 6
-        cols = 5
+        rows = picking_data["magnet_per_row"]
+        cols = picking_data["number_of_row"]
 
         for i in range(rows):
             for j in range(cols):
@@ -23,14 +38,18 @@ def main():
                 marker.id = i * cols + j  # Unique ID for each marker
                 marker.type = Marker.SPHERE
                 marker.action = Marker.ADD
-                marker.pose.position.x = 0.405 + j * 0.05  # Adjust the x position for each marker
-                marker.pose.position.y = -0.107 - i * 0.035  # Adjust the y position for each marker
-                marker.pose.position.z = 0
+                marker.pose.position.x = (
+                    picking_data["first_magnet"]["x"] + j * 0.05
+                )
+                marker.pose.position.y = (
+                    picking_data["first_magnet"]["y"] - i * 0.035
+                )
+                marker.pose.position.z = picking_data["first_magnet"]["z"] +0.01
                 marker.pose.orientation.x = 0.0
                 marker.pose.orientation.y = 0.0
                 marker.pose.orientation.z = 0.0
                 marker.pose.orientation.w = 1.0
-                marker.scale.x = 0.01  # Diameter of the sphere in meters (10 mm)
+                marker.scale.x = 0.01  # Convert to meters
                 marker.scale.y = 0.01
                 marker.scale.z = 0.01
                 marker.color.a = 1.0
